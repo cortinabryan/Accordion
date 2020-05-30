fe1bf3f5b7f433d92bab0900204441a46559546f
cortinabryan <64225493+cortinabryan@users.noreply.github.com>
2020-05-30 08:21:33 -0700

    Added + & - signs

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ function accordion() {
     let accHeader = item.firstElementChild;
     accHeader.addEventListener("click", function(){
      let accDetails = this.nextElementSibling; 
+     if (accDetails.style.maxHeight) {
+       accDetails.style.maxHeight = null;
+       this.lastElementChild.innerHTML = "+";
+     } else {
+       accDetails.style.maxHeight = accDetails.scrollHeight + "px";
+       this.lastElementChild.innerHTML = "-";
+     }
     })
   }
 }
